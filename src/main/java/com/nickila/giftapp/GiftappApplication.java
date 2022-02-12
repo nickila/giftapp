ecb801e19ba81c5280bcbf50c7e355efd88e3a4e
Brian Nickila <brian.nickila@gmail.com>
2022-02-11 22:22:44 -0600

    Initial commit

---
+++ src/main/java/com/nickila/giftapp/GiftappApplication.java
@@ -0,0 +1,13 @@
+package com.nickila.giftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickila/giftapp/GiftappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickila.giftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
